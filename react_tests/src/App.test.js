4cf7c185efa5efbd31b2c5112eb42088a22a0421
IgorLapuzo <104383591+IgorLapuzo@users.noreply.github.com>
2022-11-15 14:38:46 +0300

    test ...By, async test, events

--- react_tests/src/App.test.js
+++ react_tests/src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
 describe('TEST APP', () => {
@@ -10,8 +10,39 @@ describe('TEST APP', () => {
     expect(helloWorldElem).toBeInTheDocument();
     expect(btn).toBeInTheDocument();
     expect(input).toBeInTheDocument();
-    screen.debug()
-    expect(input).toMatchSnapshot()
+    screen.debug();
+    expect(input).toMatchSnapshot();
+  });
+
+  test('renders learn react link', async() => {
+    render(<App />);
+    //const helloWorldElem = screen.queryByText(/hello2/i);
+    //expect(helloWorldElem).toBeNull();
+    screen.debug();
+    const helloWorldElem = await screen.findByText(/data/i);
+    expect(helloWorldElem).toBeInTheDocument();
+    expect(helloWorldElem).toHaveStyle({color: 'red'});
+    screen.debug();
+  });
+
+  test('CLICK EVENT', () => {
+    render(<App />);
+    const btn = screen.getByTestId('toggle-btn');
+    expect(screen.queryByTestId('toggle-elem')).toBeNull();
+    fireEvent.click(btn);
+    expect(screen.queryByTestId('toggle-elem')).toBeInTheDocument();
+    fireEvent.click(btn);
+    expect(screen.queryByTestId('toggle-elem')).toBeNull();
+  });
+
+  test('INPUT EVENT', () => {
+    render(<App />);
+    const input = screen.getByPlaceholderText(/input value/i); 
+    expect(screen.queryByTestId('value-elem')).toContainHTML('');
+    fireEvent.input(input, {
+      target: {value: '123'}
+    });
+    expect(screen.queryByTestId('value-elem')).toContainHTML('123');
   });
 })
 
