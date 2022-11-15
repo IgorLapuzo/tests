9f7641cdc7e6ffddd97f589f710f48b40846b880
IgorLapuzo <104383591+IgorLapuzo@users.noreply.github.com>
2022-11-15 12:57:16 +0300

    init react app, work with test lib

---
+++ react_tests/src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <button>click me</button>
+      <input type='text' placeholder='input value'></input>
+    </div>
+  );
+}
+
+export default App;
---
+++ react_tests/src/App.test.js
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+describe('TEST APP', () => {
+  test('renders learn react link', () => {
+    render(<App />);
+    const helloWorldElem = screen.getByText(/hello world/i);
+    const btn = screen.getByRole('button');
+    const input = screen.getByPlaceholderText(/input value/i);
+    expect(helloWorldElem).toBeInTheDocument();
+    expect(btn).toBeInTheDocument();
+    expect(input).toBeInTheDocument();
+    screen.debug()
+    expect(input).toMatchSnapshot()
+  });
+})
+
